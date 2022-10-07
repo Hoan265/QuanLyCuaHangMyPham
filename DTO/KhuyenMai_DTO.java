1c101f1fde8c9c4ed91a5ec0c4ceca9594c1e461
HaLong122002 <tranthihalong2002@gmail.com>
2022-10-07 20:14:10 +0700

    Long: Khuyến Mãi, Khách Hàng DTO

---
+++ DTO/KhuyenMai_DTO.java
@@ -0,0 +1,77 @@
+package icy.DTO;
+
+public class KhuyenMai_DTO {
+    public String makm;
+    public String ten;
+    public String ngaybd;
+    public String thangbd;
+    public String nambd;
+    public String ngaykt;
+    public String thangkt;
+    public String namkt;
+    
+
+    public String getMakm() {
+        return makm;
+    }
+
+    public void setMakm(String makm) {
+        this.makm = makm;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getNgaybd() {
+        return ngaybd;
+    }
+
+    public void setNgaybd(String ngaybd) {
+        this.ngaybd = ngaybd;
+    }
+
+    public String getThangbd() {
+        return thangbd;
+    }
+
+    public void setThangbd(String thangbd) {
+        this.thangbd = thangbd;
+    }
+
+    public String getNambd() {
+        return nambd;
+    }
+
+    public void setNambd(String nambd) {
+        this.nambd = nambd;
+    }
+
+    public String getNgaykt() {
+        return ngaykt;
+    }
+
+    public void setNgaykt(String ngaykt) {
+        this.ngaykt = ngaykt;
+    }
+
+    public String getThangkt() {
+        return thangkt;
+    }
+
+    public void setThangkt(String thangkt) {
+        this.thangkt = thangkt;
+    }
+
+    public String getNamkt() {
+        return namkt;
+    }
+
+    public void setNamkt(String namkt) {
+        this.namkt = namkt;
+    }
+}
