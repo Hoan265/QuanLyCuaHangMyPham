84b46472eb14f7bb81ad9cf22668ae73bc47ed8d
Myyly <myylyy12@gmail.com>
2022-10-09 20:22:00 +0700

    Ly: Nhân viên - Quản Lý tài khoản DAO

---
+++ DAO/DangKy_DAO.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package icy.DAO;
+
+import ICY.DTO.DangKyDTO;
+import icy.GUI.QLNV_GUI;
+import icy.SQLConnect;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Quan Phat Vinh Thanh
+ */
+public class DangKy_DAO {
+    SQLConnect connect;
+
+    public DangKy_DAO() {
+        connect = new SQLConnect("DESKTOP-1PTP5JE", "ICY", "sa", "123");
+    }
+
+    public ArrayList DocDSDK() {
+        ArrayList dsdk = new ArrayList<DangKyDTO>();
+        try {
+            String qry = "select * from NguoiDung";
+            ResultSet result = connect.executeQuery(qry);
+            while (result.next()) {
+                DangKyDTO dk = new DangKyDTO();
+                dk.MaDK = result.getString(1);
+                dk.TaiKhoan = result.getString(2);
+                dk.MatKhau = result.getString(3);
+                dk.ChuVuDK = result.getString(4);
+                dsdk.add(dk);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(QLNV_GUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return dsdk;
+    }
+
+    public void themTK(DangKyDTO dk) {
+        try {
+            String qry = "Insert into NguoiDung Values (";
+            qry = qry + " N'" + dk.MaDK + "'";
+            qry = qry + ",N" + "'" + dk.TaiKhoan + "'";
+            qry = qry + ",N" + "'" + dk.MatKhau + "'";
+            qry = qry + ",N" + "'" + dk.ChuVuDK + "'";
+            qry = qry + ")";
+            connect.executeUpdate(qry);
+        } catch (Exception ex) {
+            Logger.getLogger(QLNV_GUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void xoaTK(String ma) {
+        try {
+            String qry = "Delete from NguoiDung WHERE Ma=N'" + ma + "'";
+            connect.executeUpdate(qry);
+        } catch (Exception ex) {
+            Logger.getLogger(QLNV_GUI.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("xoa that bai");
+        }
+    }
+}
---
+++ DAO/DangNhap_DAO.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ICY.DAO;
+
+import ICY.DTO.DangKyDTO;
+import icy.GUI.DangNhap_GUI;
+import icy.GUI.QLNV_GUI;
+import icy.SQLConnect;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author HP
+ */
+public class DangNhap_DAO {
+
+    static ArrayList dsdk = new ArrayList<DangKyDTO>();
+    static ArrayList test = new ArrayList<DangKyDTO>();
+    DangKyDTO dk = new DangKyDTO();
+    SQLConnect connect;
+    public DangNhap_DAO() {
+       connect = new SQLConnect("DESKTOP-1PTP5JE", "ICY", "sa", "123");
+    }
+
+    public ArrayList DocDSDN() {
+
+        try {
+            String qry = "select * from NguoiDung";
+            ResultSet result = connect.executeQuery(qry);
+            while (result.next()) {
+                DangKyDTO dk = new DangKyDTO();
+                dk.MaDK = result.getString(1);
+                dk.TaiKhoan = result.getString(2);
+                dk.MatKhau = result.getString(3);
+                dk.ChuVuDK = result.getString(4);
+                dsdk.add(dk);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(DangNhap_GUI.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("chua ket noi");
+        }
+        return dsdk;
+    }
+    public ArrayList DangNhap(String x, String y) {
+        DangKyDTO dk = new DangKyDTO();
+        try {
+            String qry = "select * from NguoiDung where TaiKhoan=N'" + x + "'and MatKhau=N'" + y + "'"; //where Ten= N'"+dn.TenDangNhap+"'";
+            ResultSet result = connect.executeQuery(qry);
+            while (result.next()) {
+                dk.MaDK = result.getString(1);
+                dk.TaiKhoan = result.getString(2);
+                dk.MatKhau = result.getString(3);
+                dk.ChuVuDK = result.getString(4);
+                test.add(dk);
+                System.out.println(test.size());
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(DangNhap_GUI.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("chua ket noi");
+        }
+        return test;
+    }
+
+    public void sua(String x, String y) {
+        try {
+            String qry = "Update NguoiDung Set";
+            qry = qry + " " + "MatKhau=" + "N'" + x + "'";
+            qry = qry + " Where Ma=" + "N'" + y + "'";
+            connect.executeUpdate(qry);
+        } catch (Exception ex) {
+            Logger.getLogger(QLNV_GUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
